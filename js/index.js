5a8a18212187a7bc5aa6027b37d005bbb1e6b06c
Stanislav M <verve111@mail.ru>
2015-04-02 15:40:23 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,78 @@
+/* ===== Tooltips ===== */
+
+$('#tooltip').tooltip();
+
+function getURLParameter(sParam) {
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+	for (var i = 0; i < sURLVariables.length; i++) {
+		var sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] == sParam) {
+			return sParameterName[1];
+		}
+	}
+	return null;
+}
+
+function isRu() {
+	var lang = navigator.languages ? navigator.languages[0]
+			: (navigator.language || navigator.userLanguage);
+	if (lang === 'ru' || lang === 'ru-RU') {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+$('#blog_navnar.navbar.navbar-default.navbar-fixed-top a[href!="#"]').each(
+	function() {
+		var href = this.href;
+		var sourceLang;
+		if (isRu()) {
+			sourceLang = '';
+		} else {
+			sourceLang = 'en/';
+		}
+		this.href = href.replace('blog/', sourceLang);
+	});
+
+$('#blog_navnar.navbar.navbar-default.navbar-fixed-top a').each(
+	function() {
+		if (!isRu()) {
+			return;
+		}
+		var h = $(this).html();
+		if (h.indexOf('Home') == 0) {
+			$(this).html(h.replace('Home', 'Главная'));
+		}
+		if (h.indexOf('Company') == 0) {
+			$(this).html(h.replace('Company', 'Компания'));
+		}		
+		if (h.indexOf('About us') == 0) {
+			$(this).html(h.replace('About us', 'O компании'));
+		}	
+		if (h.indexOf('Contacts') == 0) {
+			$(this).html(h.replace('Contacts', 'Контакты'));
+		}	
+		if (h.indexOf('Solutions') == 0) {
+			$(this).html(h.replace('Solutions', 'Продукты'));
+		}		
+		if (h.indexOf('ECM Alfresco') == 0) {
+			$(this).html(h.replace('ECM Alfresco', 'Внедрение СЭД Alfresco'));
+		}			
+		if (h.indexOf('Enterprise Portal Liferay') == 0) {
+			$(this).html(h.replace('Enterprise Portal Liferay', 'Корпоративый портал Liferay'));
+		}	
+		if (h.indexOf('Services') == 0) {
+			$(this).html(h.replace('Services', 'Услуги'));
+		}		
+		if (h.indexOf('Enterprise software') == 0) {
+			$(this).html(h.replace('Enterprise software', 'Корпоративные ИТ-продукты'));
+		}	
+		if (h.indexOf('IT Consulting') == 0) {
+			$(this).html(h.replace('IT Consulting', 'ИТ-Консалтинг'));
+		}		
+		if (h.indexOf('Blog') == 0) {
+			$(this).html(h.replace('Blog', 'Блог'));
+		}		
+	});
---
+++ readme.txt
@@ -0,0 +1,5 @@
+1. to apply changes to .vm: restart tomcat, set custom theme to soft29
+2. to apply changes to .js: set custom theme to soft29, without tomcat restart
+3. all resources (css js)should be added to theme.xml. Only then import is possible.
+4. main menu: http://localhost:8080/soft29/roller-ui/menu.rol
+5. a hrefs in blog substituted in index.js
